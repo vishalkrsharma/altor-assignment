519a52614f4b53e00d20765e01dc64afe0e11ee2
vishalkrsharma <vishalkrsharma1234@gmail.com>
2024-02-24 15:36:59 +0530

    chore: api implementation wrapped inside try catch

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -2,16 +2,16 @@ import db from '@/lib/db';
 import { NextResponse } from 'next/server';
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const zone = searchParams.get('zone') || undefined;
-  const device_brand = searchParams.get('device_brand') || undefined;
-  const vehicle_brand = searchParams.get('vehicle_brand') || undefined;
-  const vehicle_cc = searchParams.get('vehicle_cc') || undefined;
-  const sdk_int = searchParams.get('sdk_int') || undefined;
-
-  let sdk_int_num = sdk_int ? parseInt(sdk_int) : undefined;
-
   try {
+    const { searchParams } = new URL(req.url);
+    const zone = searchParams.get('zone') || undefined;
+    const device_brand = searchParams.get('device_brand') || undefined;
+    const vehicle_brand = searchParams.get('vehicle_brand') || undefined;
+    const vehicle_cc = searchParams.get('vehicle_cc') || undefined;
+    const sdk_int = searchParams.get('sdk_int') || undefined;
+
+    let sdk_int_num = sdk_int ? parseInt(sdk_int) : undefined;
+
     const data = await db.data.findMany({
       where: {
         zone,
